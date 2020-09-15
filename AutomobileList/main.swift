0e742f91216c87c765d18cb891754cf127b28363
Ruslan Abbasov <abbasov_ruslan1999@icloud.com>
2020-09-15 10:45:28 +0700

    Initial Commit

---
+++ AutomobileList/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AutomobileList
+//
+//  Created by Ruslan Abbasov on 15.09.2020.
+//  Copyright © 2020 Ruslan Abbasov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
